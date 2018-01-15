ef877206a4516732379ebe1203edd1b41b8906e7
Ranjan Adhikari <adh.ranjan@gmail.com>
2018-01-15 13:27:30 +0530

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,25 +1,62 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id:10
-}
+var password = 'avdd12!@#';
 
-var token = jwt.sign(data,'123abc')
-var data = jwt.verify(token,'123abc')
-console.log(data)
+  bcrypt.genSalt(10,(error,salt)=>{
+    bcrypt.hash(password,salt,(error,hash)=>{
+      console.log(hash)
+    })
+  })
 
 
-var data = {
-  id:4
-}
 
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data)+'my_key').toString()
-}
 
-token.data.id=5
 
-var result = SHA256(JSON.stringify(token.data)+'my_key').toString()
-console.log(result === token.hash)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var data = {
+//   id:10
+// }
+//
+// var token = jwt.sign(data,'123abc')
+// var data = jwt.verify(token,'123abc')
+// console.log(data)
+//
+//
+// var data = {
+//   id:4
+// }
+//
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data)+'my_key').toString()
+// }
+//
+// token.data.id=5
+//
+// var result = SHA256(JSON.stringify(token.data)+'my_key').toString()
+// console.log(result === token.hash)
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
 
 var UserSchema = new mongoose.Schema({
   email:{
@@ -59,6 +61,26 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+
+UserSchema.pre('save',function(next){
+  var user = this
+
+  if(user.isModified('password')){
+
+    bcrypt.genSalt(10,(error,salt)=>{
+      bcrypt.hash(user.password,salt,(error,hash)=>{
+        user.password = hash
+        next()
+      })
+    })
+
+  }else{
+    next()
+  }
+
+
+
+})
 
 var User = mongoose.model('Users',UserSchema)
 
